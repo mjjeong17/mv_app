1b40a973aed1e51b36a6d851e13905de7a944b0e
mjjeong <mjjeong17@gmail.com>
2020-12-14 16:46:15 +0900

    컴포넌트 - 재사용 테스트

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+function Food({fav}){
+  return (
+    <h1>I Like {fav}</h1>
+  ); 
+}
+
 function App() {
-  return <div>Hello!!</div>;
+  return (
+    <div>
+      <h1>Hello!!</h1>
+      <Food fav="kimchi"/>
+      <Food fav="apple"/>
+      <Food fav="banana"/>
+    </div>
+  );
 }
 
 export default App;
